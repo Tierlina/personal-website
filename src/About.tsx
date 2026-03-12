7c71fb7cd5c3ed2d8cc378a585e2e1e3d85fafef
unknown <tierlinapayne@gmail.com>
2026-03-12 17:01:07 +1100

    update image

--- src/About.tsx
+++ src/About.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@mantine/core';
 import {Tab} from './TabComponent';
+import Tierlina from './assets/Tierlina.png'
 
 export function About() {
   return (
@@ -11,7 +12,7 @@ export function About() {
         </div>
 
         <div className="flex flex-col md:flex-row  ml-[10vw] md:ml-[5vw]">
-          <img src="src\assets\Tierlina.png" className='size-60 border-5 rounded-xl' />
+          <img src={Tierlina} className='size-60 border-5 rounded-xl' />
           <div className=" mb-20 text-center text-xl w-[80vw] md:w-[60vw] md:ml-[10vw] mt-[10vw] md:mt-0 p-[5vw] bg-pink border-5 rounded-xl">
 I am a software developer driven by the intersection of clean code and human impact. While I’m experienced across various stacks, my true strength lies in the "soft" side of hard tech: organization, seamless collaboration, and high-level communication. <br/><br/>
 
